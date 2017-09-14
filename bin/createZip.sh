1e9ba8e0e7925b1545e92b38209eb4c9738859f7
teal <yaowang@huize.com>
2017-09-14 18:19:52 +0800

    delete private keywords

--- bin/createZip.sh
+++ bin/createZip.sh
@@ -41,7 +41,7 @@ do
     done
 done
 
-# 2017-07-07 09:34:00|192.168.10.104|jumi-node|2393|活动添加背景音乐需求
+# 2017-07-07 09:34:00|192.168.10.104|xx-node|2393|活动添加背景音乐需求
 sqlite3 /home/teal/svnPackage/db/main.sqlite3 "insert into access_log values (NULL, '$accessTime', '$ip', '$appName', '$revision', '$comments')"
 
 cd /home/teal/svnPackage/$exportPath/$dirName
